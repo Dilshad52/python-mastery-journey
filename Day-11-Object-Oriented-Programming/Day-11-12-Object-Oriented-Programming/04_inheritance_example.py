3ad6d9f6209a1679b0ea2e21eb24748be51d661e
Mohammed Dilshad <dilshadmohammed547@gmail.com>
2026-06-14 13:27:53 +0530

    Create 04_inheritance_example.py

---
+++ Day-11-Object-Oriented-Programming/Day-11-12-Object-Oriented-Programming/04_inheritance_example.py
@@ -0,0 +1,23 @@
+class Person:
+
+    def __init__(self, name):
+        self.name = name
+
+    def show(self):
+        print("Name:", self.name)
+
+
+class Student(Person):
+
+    def __init__(self, name, marks):
+        super().__init__(name)
+        self.marks = marks
+
+    def display(self):
+        self.show()
+        print("Marks:", self.marks)
+
+
+s = Student("Dilshad", 95)
+
+s.display()
