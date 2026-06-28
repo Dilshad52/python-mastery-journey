26b40f2dd63769340305096d9cab14a970f8f761
Mohammed Dilshad <dilshadmohammed547@gmail.com>
2026-06-28 20:10:22 +0530

    Add tree traversal functions and example usage

---
+++ Day24-Trees-and-BST/tree_traversals.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+def inorder(root):
+    if root:
+        inorder(root.left)
+        print(root.data, end=" ")
+        inorder(root.right)
+
+
+def preorder(root):
+    if root:
+        print(root.data, end=" ")
+        preorder(root.left)
+        preorder(root.right)
+
+
+def postorder(root):
+    if root:
+        postorder(root.left)
+        postorder(root.right)
+        print(root.data, end=" ")
+
+
+root = Node(10)
+root.left = Node(5)
+root.right = Node(15)
+root.left.left = Node(2)
+root.left.right = Node(7)
+
+print("Inorder:")
+inorder(root)
+
+print("\nPreorder:")
+preorder(root)
+
+print("\nPostorder:")
+postorder(root)
